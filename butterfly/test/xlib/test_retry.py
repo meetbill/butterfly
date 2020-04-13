fcd3f6f67e131c9d89f58a297dba0edcf654eaac
meetbill <meetbill@163.com>
2020-04-13 11:24:31 +0800

    [Added]: add retry util

---
+++ butterfly/test/xlib/test_retry.py
@@ -0,0 +1,123 @@
+from datetime import datetime
+from threading import Thread
+from time import sleep
+
+import pytest
+
+from xlib.util import retry
+
+exception_message = 'testing exceptions'
+
+
+def foo(bar):
+    if bar < 0:
+        raise ArithmeticError(exception_message)
+    return bar
+
+
+def test_success_criteria():
+    """Success criteria successfully raises MaximumRetriesExceeded"""
+    foo_with_success = retry.retry(success=lambda x: x > 0)(foo)
+    with pytest.raises(retry.MaximumRetriesExceeded):
+        foo_with_success(0)
+
+
+def test_exception_criteria():
+    """Exceptions specified are raised on MaximumRetriesExceeded"""
+    foo_with_exception = retry.retry(exceptions=(ArithmeticError,))(foo)
+    with pytest.raises(ArithmeticError) as exc_info:
+        foo_with_exception(-1)
+    assert exception_message in str(exc_info.value)
+
+
+def test_execution():
+    """Expected execution of a successful runstill works"""
+    foo_with_both = retry.retry(
+        exceptions=(ArithmeticError,), success=lambda x: x > 0)(foo)
+    assert foo_with_both(1) == 1
+
+
+def test_interval():
+    """Interval expected is the interval to complete an action"""
+    def _success_interval(in_dict):
+        in_dict['num'] += 1
+        return in_dict['num']
+
+    baz_with_interval = retry.retry(
+        success=lambda x: x > 5, interval=1)(_success_interval)
+    start = datetime.now()
+    baz_with_interval({'num': 0})
+    elapsed = datetime.now() - start
+    assert elapsed.seconds >= 5
+
+
+def test_invalid_parameters():
+    """The exceptions and success parameter can not both be None"""
+    with pytest.raises(TypeError):
+        retry.retry(exceptions=None, success=None)(foo)
+
+
+def test_unsuccessful_timeout():
+    """Unsuccessful functions with a timeout work"""
+    foo_with_timeout = retry.retry(
+        success=lambda x: x > 0, timeout=5, interval=1)(foo)
+    with pytest.raises(retry.MaximumTimeoutExceeded):
+        foo_with_timeout(-1)
+
+
+def test_successful_timeout():
+    """Success with a timeout still works"""
+    def _success_timeout(in_dict):
+        in_dict['num'] += 1
+        return in_dict['num']
+
+    try:
+        _test_func = retry.retry(
+            success=lambda x: x == 5, timeout=10, interval=1)(
+                _success_timeout)
+        _test_func({'num': 0})
+    except retry.MaximumTimeoutExceeded:
+        pytest.fail('Expected the timeout not to be exceeded')
+
+
+def test_disarm_signal_on_success():
+    """Success with a timeout disarms signal"""
+    _test_func = retry.retry(success=lambda x: True, timeout=1, interval=0.5)(foo)
+    _test_func(1)
+    sleep(1.2)
+
+
+def test_successful_thread():
+    """Success with function as thread"""
+    retryed = []
+
+    @retry.retry(timeout=1, success=lambda x: len(x) == 3)
+    def f(retryed):
+        retryed.append(0)
+        return retryed
+
+    t = Thread(target=f, args=[retryed])
+    t.start()
+    t.join()
+    assert 3 == len(retryed)
+
+
+def test_unsuccessful_thread():
+    """Unsuccessful with function as thread, timed out"""
+    retryed = []
+
+    def foo(retryed):
+        @retry.retry(timeout=1, success=lambda x: False)
+        def bar(retryed):
+            sleep(0.2)
+            retryed.append(0)
+
+        with pytest.raises(retry.MaximumTimeoutExceeded):
+            bar(retryed)
+
+    t = Thread(target=foo, args=[retryed])
+    t.start()
+    t.join()
+    assert 3 <= len(retryed) <= 5
+
+
---
+++ butterfly/xlib/util/retry.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+# coding=utf8
+"""
+# Author: meetbill
+# Created Time : 2020-04-13 09:46:41
+
+# File Name: retry.py
+# Description: A simple python module to add a retry function decorator
+
+可以根据以下条件触发重试：
+    成功条件（例如状态码不等于200）
+    异常（例如引发 request.RequestException）
+
+被装饰函数可能基于以下原因而执行失败：
+    超过最大重试次数
+    达到最大超时
+
+被装饰函数可以按特定间隔进行下次重试
+
+base: https://github.com/seemethere/retry.it
+"""
+import functools
+import itertools
+import logging
+import time
+
+from decorator import decorator
+
+
+class _DummyException(Exception):
+    pass
+
+
+class MaximumRetriesExceeded(Exception):
+    """
+    重试次数用完，返回的异常
+    """
+    pass
+
+
+class MaximumTimeoutExceeded(Exception):
+    """
+    执行耗时超时返回的异常
+    """
+    pass
+
+
+def retry(exceptions=(Exception,), interval=0, max_retries=10, success=None, timeout=-1):
+    """Decorator to retry a function 'max_retries' amount of times
+
+    :param tuple exceptions: Exceptions to be caught for retries
+    :param int interval: Interval between retries in seconds
+    :param int max_retries: Maximum number of retries to have, if
+        set to -1 the decorator will loop forever
+    :param function success: Function to indicate success criteria
+    :param int timeout: Timeout interval in seconds, if -1 will retry forever
+    :raises MaximumRetriesExceeded: Maximum number of retries hit without reaching the success criteria
+    :raises TypeError: Both exceptions and success were left None causing the
+        decorator to have no valid exit criteria.
+
+    Example:
+        Use it to decorate a function!
+
+        .. sourcecode:: python
+
+            from retry import retry
+
+            @retry(exceptions=(ArithmeticError,), success=lambda x: x > 0)
+            def foo(bar):
+                if bar < 0:
+                    raise ArithmeticError('testing this')
+                return bar
+
+            foo(5)
+            # Should return 5
+            foo(-1)
+            # Should raise ArithmeticError
+            foo(0)
+            # Should raise MaximumRetriesExceeded
+    """
+    if not exceptions and success is None:
+        raise TypeError('`exceptions` and `success` parameter can not both be None')
+
+    # For python 3 compatability
+    exceptions = exceptions or (_DummyException,)
+    _retries_error_msg = '[retry] Exceeded maximum number of retries {} at an interval of {}s for function {}'
+    _timeout_error_msg = '[retry] Maximum timeout of {}s reached for function {}'
+
+    @decorator
+    def wrapper(func, *args, **kwargs):
+        run_func = functools.partial(func, *args, **kwargs)
+        logger = logging.getLogger(func.__module__)
+
+        if max_retries < 0:
+            iterator = itertools.count()
+        else:
+            iterator = range(max_retries)
+
+        t_beginning = time.time()
+
+        for num, _ in enumerate(iterator, 1):
+            try:
+                result = run_func()
+                if success is None or success(result):
+                    return result
+
+            except exceptions:
+                logger.exception('[retry] Exception experienced when trying function {}'.format(func.__name__))
+
+                # 所装饰函数体中有返回异常，且执行次数达到上限，进行返回异常
+                if num == max_retries:
+                    raise
+
+            logger.warning('[retry] Retrying {} in {}s...'.format(func.__name__, interval))
+            seconds_passed = time.time() - t_beginning
+
+            if timeout > 0 and seconds_passed > timeout:
+                # 若配置了 timeout，且所装饰函数执行时间超过了 timeout，则返回 MaximumTimeoutExceeded 异常
+                raise MaximumTimeoutExceeded(_timeout_error_msg.format(timeout, func.__name__))
+            else:
+                time.sleep(interval)
+
+        # 循环次数用完, 所装饰函数本身没有返回异常，但返回结果不符合预期，返回 MaximumRetriesExceeded 异常
+        raise MaximumRetriesExceeded(_retries_error_msg.format(max_retries, interval, func.__name__))
+    return wrapper
