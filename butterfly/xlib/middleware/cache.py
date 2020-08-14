6c6fc7e5a0c85e84f1bc8f411b732e31021e09a6
meetbill <meetbill@163.com>
2020-08-14 14:16:44 +0800

    [Fixed]: _cache.memoiz => _cache.memoize

--- butterfly/xlib/middleware/cache.py
+++ butterfly/xlib/middleware/cache.py
@@ -7,13 +7,17 @@
 # File Name: cache.py
 # Description:
 
+---------------------------------------------------- example
+from xlib.middleware.cache import cache_page
+
 # 缓存 1 秒
 @cache_page(expire=1)
 def generate_landing_page():
     return 3
+----------------------------------------------------
 """
 
 from xlib import diskcache as _dc
 
 _cache = _dc.Cache()
-cache_page = _cache.memoiz
+cache_page = _cache.memoize
