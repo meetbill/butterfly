6390d66a5b3fe9418285198612e04a5edf2a4823
meetbill <meetbill@163.com>
2021-03-23 14:52:53 +0800

    [Changed]: 配置中日志和 data 目录从相对路径修改为绝对路径

--- butterfly/conf/config.py
+++ butterfly/conf/config.py
@@ -2,6 +2,10 @@
 """
 Butterfly config
 """
+import os
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SERVER_LISTEN_ADDR = ("0.0.0.0", 8585)
 SERVER_THREAD_NUM = 16
@@ -10,18 +14,18 @@ SERVER_NAME = "Butterfly_app"
 # Log
 LOG_SIZE_LIMIT = 1024 * 1024 * 2
 LOG_BATCH_WRITE = 0
-PATH_INIT_LOG = "logs/init.log"
-PATH_ACC_LOG = "logs/acc.log"
-PATH_INFO_LOG = "logs/info.log"
-PATH_WARNING_LOG = "logs/warning.log"
-PATH_ERR_LOG = "logs/err.log"
-PATH_CRIT_LOG = "logs/crit.log"
-PATH_COMMON_LOG = "logs/common.log"
-PATH_COMMON_BF_LOG = "logs/common_bf.log"
+PATH_INIT_LOG = os.path.join(BASE_DIR, "logs/init.log")
+PATH_ACC_LOG = os.path.join(BASE_DIR, "logs/acc.log")
+PATH_INFO_LOG = os.path.join(BASE_DIR, "logs/info.log")
+PATH_WARNING_LOG = os.path.join(BASE_DIR, "logs/warning.log")
+PATH_ERR_LOG = os.path.join(BASE_DIR, "logs/err.log")
+PATH_CRIT_LOG = os.path.join(BASE_DIR, "logs/crit.log")
+PATH_COMMON_LOG = os.path.join(BASE_DIR, "logs/common.log")
+PATH_COMMON_BF_LOG = os.path.join(BASE_DIR, "logs/common_bf.log")
 
 # static
 STATIC_PATH = "static"
-STATIC_PREFIX="static"
+STATIC_PREFIX = "static"
 
 # DB
 """
@@ -29,7 +33,7 @@ STATIC_PREFIX="static"
 """
 DATABASES = {
     "default": "mysql+retrypool://root:password@127.0.0.1:3306/test?max_connections=300&stale_timeout=300",
-    }
+}
 
 # Redis
 """
@@ -39,18 +43,18 @@ DATABASES = {
 CACHES = {
     "default": "redis://@localhost:6379/0",
     # for cache: connect_timeout 100ms, read_timeout 200ms
-    #"wuxing": "redis://@localhost:6379/0?socket_timeout=0.2&socket_connect_timeout=0.1&retry_on_timeout=false",
+    # "wuxing": "redis://@localhost:6379/0?socket_timeout=0.2&socket_connect_timeout=0.1&retry_on_timeout=false",
     # for mq: connect_timeout 500ms, read_timeout 2000ms
-    #"baichuan": "redis://@localhost:6379/0?socket_timeout=2&socket_connect_timeout=0.5&retry_on_timeout=false",
-    }
+    # "baichuan": "redis://@localhost:6379/0?socket_timeout=2&socket_connect_timeout=0.5&retry_on_timeout=false",
+}
 
 # Local data or cache
-LOCALDATA_DIR = "data"
+LOCALDATA_DIR = os.path.join(BASE_DIR, "data")
 
 # Auth
-SECRET_KEY = None           # If it is None, a key will be randomly generated each time butterfly is started
-JWT_TOKEN_TTL = 28800       # default 8 hours
+SECRET_KEY = None               # If it is None, a key will be randomly generated each time butterfly is started
+JWT_TOKEN_TTL = 28800           # default 8 hours
 
 # Scheduler
-scheduler_name="Scheduler1" # Scheduler name, Used to perform historical queries
-scheduler_store="memory"    # ("mysql"/"memory")
+scheduler_name = "Scheduler1"   # Scheduler name, Used to perform historical queries
+scheduler_store = "memory"      # ("mysql"/"memory")
